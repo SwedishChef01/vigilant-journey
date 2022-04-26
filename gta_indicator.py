6a9afc38736271d1a9ee5dfaf6a0de64b641c4e6
Graeme Johnston <gpjohno1@icloud.com>
2022-04-26 12:32:52 +0100

    Lunchtime commit.

--- gta_indicator.py
+++ gta_indicator.py
@@ -4,33 +4,33 @@ import pandas as pd
 
 def donchian(prices, period):
     '''Calculate upper, lower, & middle Donchian channels.'''
-    df = pd.DataFrame(prices)
-    df["upr"] = df.high.rolling(period).max().shift(periods=1)
-    df["lwr"] = df.low.rolling(period).min().shift(periods=1)
-    df["mid"] = 0.5 * (df.upr + df.lwr)
+    df = pd.DataFrame()
+    df[f'upr{period}'] = prices.high.rolling(period).max().shift(periods=1)
+    df[f'lwr{period}'] = prices.low.rolling(period).min().shift(periods=1)
+    df[f'mid{period}'] = 0.5 * (df[f'upr{period}'] + df[f'lwr{period}'])
     return df
 
 
 def rsi(close, period):
     """Relative Strength Index (RSI)."""
     df = pd.DataFrame(close)
-    df["chg"] = df.close.diff()
-    df["gain"] = df.chg.clip(lower=0)
-    df["loss"] = df.chg.clip(upper=0).abs()
-    df["avg_gain"] = np.nan
-    df["avg_loss"] = np.nan
+    df['chg'] = df.close.diff()
+    df['gain'] = df.chg.clip(lower=0)
+    df['loss'] = df.chg.clip(upper=0).abs()
+    df['avg_gain'] = np.nan
+    df['avg_loss'] = np.nan
     df.avg_gain.iloc[period] = df.gain[0:period].sum() / period
     df.avg_loss.iloc[period] = df.loss[0:period].sum() / period
 
     for i in range(period + 1, df.shape[0]):
-        df["avg_gain"].iloc[i] = (
-            df["avg_gain"].iloc[i - 1] * (period - 1) + df["gain"].iloc[i]
+        df['avg_gain'].iloc[i] = (
+            df['avg_gain'].iloc[i - 1] * (period - 1) + df['gain'].iloc[i]
         ) / period
 
-        df["avg_loss"].iloc[i] = (
-            df["avg_loss"].iloc[i - 1] * (period - 1) + df["loss"].iloc[i]
+        df['avg_loss'].iloc[i] = (
+            df['avg_loss'].iloc[i - 1] * (period - 1) + df['loss'].iloc[i]
         ) / period
 
-    df["rs"] = df.avg_gain / df.avg_loss
-    df["rsi"] = 100 - 100 / (1 + df.rs)
+    df['rs'] = df.avg_gain / df.avg_loss
+    df['rsi'] = 100 - 100 / (1 + df.rs)
     return df.rsi
